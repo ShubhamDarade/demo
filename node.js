f924eca085071d396e5ed622b2fcc7f66458a035
Shubham Darade <138992223+ShubhamDarade@users.noreply.github.com>
2023-10-03 00:13:24 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,29 @@
+// const fs = require('fs')
+// let text = fs.readFileSync('read.txt', 'utf-8')
+// console.log(`the content of file is : ${text}`)
+
+// const fs = require('fs')
+// let text = fs.readFileSync('read.txt', 'utf-8')
+// text = text.replace('rohan', 'shubham')
+// console.log(`the content of file is : ${text}`)
+// fs.writeFileSync('write.txt', text) // create new file names write and has content of text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
